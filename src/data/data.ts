84802bdfb54d44fe50833323f0d95db74bc2ff02
biduedson <biduzao.bidu21@gmail.com>
2024-01-08 17:04:30 -0300

    feat: create provisory data

---
+++ src/data/data.ts
@@ -0,0 +1,12 @@
+interface Categories {
+  id: number;
+  name: string;
+}
+
+export const categoriesData: Categories[] = [
+  { id: 1, name: "Colares" },
+  { id: 2, name: "Brincos" },
+  { id: 3, name: "Pulseiras" },
+  { id: 4, name: "Anéis" },
+  { id: 5, name: "Acessórios" },
+];
