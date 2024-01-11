08338e1c8a67b44af19ae16fadb547d2e6dd6452
Edson Gomes <biduzao.bidu21@gmail.com>
2024-01-11 16:39:23 -0300

    fix:add responsivit of categories

--- src/app/(home)/components/categories.tsx
+++ src/app/(home)/components/categories.tsx
@@ -3,9 +3,9 @@ import CategoryItem from "./category-item";
 
 const Categories = () => {
   return (
-    <section className=" grid grid-cols-2 gap-x-4 gap-y-2 lg:grid-cols-5">
-      {categoriesData.map((category) => (
-        <CategoryItem key={category.id} category={category} />
+    <section className="grid grid-cols-2 gap-x-4 gap-y-2 lg:grid-cols-5">
+      {categoriesData.map((category,index) => (
+        <CategoryItem key={category.id} category={category} adtionalClass={index === categoriesData.length -1? "max-lg:col-span-2":""} />
       ))}
     </section>
   );
