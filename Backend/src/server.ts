670d8af020cc83f85b95b6df938e7485e05e3003
Edson Gomes <biduzao.bidu21@gmail.com>
2024-01-11 20:04:21 -0300

    feat: create estruture of backend of project

---
+++ Backend/src/server.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import express from "express";
+//import routes from "./routes";
+
+const app = express();
+app.use(express.json());
+//app.use(routes);
+
+app.listen("4200", () => {
+  console.log("Servidor rodando na porta 4200");
+});
