88e0b562a6f66cf64a0789520bcf2b75b16ad805
Edson Gomes <biduzao.bidu21@gmail.com>
2024-01-11 16:38:59 -0300

    fix: ensure width button categories

--- src/app/(home)/components/category-item.tsx
+++ src/app/(home)/components/category-item.tsx
@@ -3,14 +3,14 @@ import { CATEGORY_ICON, Categories } from "@/data/data";
 
 interface CategoryItemsProps {
   category: Categories;
+  adtionalClass:string;
 }
 
-const CategoryItem = ({ category }: CategoryItemsProps) => {
+const CategoryItem = ({ category, adtionalClass }: CategoryItemsProps) => {
   return (
     <Badge
       variant="outline"
-      className="flex items-center justify-center gap-2 py-3"
-    >
+      className={`flex items-center justify-center gap-2 py-3 cursor-pointer ${adtionalClass}`}>
       {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
       <span className="text-xs font-bold">{category.name}</span>
     </Badge>
